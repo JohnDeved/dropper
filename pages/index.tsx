b774b65c54d8937764ccd15d56ed628500e41707
Undefined <johann@objekt.stream>
2020-07-15 12:55:13 +0200

    + rsuite

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
+import React from 'react';
+import Head from 'next/head';
+import { Uploader } from 'rsuite';
+import 'rsuite/lib/styles/themes/dark/index.less';
+
 export default function Home() {
   return (
     <div>
-      hello
+      <Head>
+        <title>Dropper</title>
+      </Head>
+
+      <Uploader action="/upload" draggable>
+        <div style={{ lineHeight: '200px' }}>Click or Drag files to this area to upload</div>
+      </Uploader>
     </div>
   )
 }
