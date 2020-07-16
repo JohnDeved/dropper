0202479f13f2a94d0f517662ef32ae9dee35980a
Undefined <johann@objekt.stream>
2020-07-16 19:52:14 +0200

    + use ext lib

--- server/routes/upload.ts
+++ server/routes/upload.ts
@@ -5,6 +5,7 @@ import Busboy from 'busboy'
 import express from 'express'
 import { move } from '../modules/rclone'
 import { postModel } from '../modules/mongo'
+import parseFile from 'parse-filepath'
 
 const router = express.Router()
 
@@ -12,13 +13,13 @@ router.post('/', (req, res) => {
   const busboy = new Busboy({ headers: req.headers })
 
   busboy.on('file', async (key, file, filename) => {
-    const [, ext] = filename.match(/\.(\w+)$/) || []
     let filehash = base64url(crypto.randomBytes(5))
-    if (ext) filehash += `.${ext}`
-    const path = `tmp/${filehash}`
+    const { ext } = parseFile(filename)
+    if (ext) filehash += ext
 
     await postModel.create({ _id: filehash, filename })
 
+    const path = `tmp/${filehash}`
     file.pipe(fs.createWriteStream(path))
 
     busboy.on('finish', async () => {
