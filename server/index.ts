4ab167fe1071b48a345070c2ceb1d368eaf1ee76
Undefined <johann@objekt.stream>
2020-07-16 18:02:26 +0200

    + clean up embed code

--- server/index.ts
+++ server/index.ts
@@ -4,8 +4,6 @@ import Next from 'next'
 import { serve } from './modules/rclone'
 import upload from './routes/upload'
 import stream from './routes/stream'
-import oembed from './routes/oembed'
-import embed from './routes/embed'
 import { dev } from './modules/config'
 
 const next = Next({ dev })
@@ -16,13 +14,8 @@ serve()
 next.prepare().then(() => {
   const app = express()
 
-  app.set('view engine', 'hbs')
-  app.set('views', path.join(__dirname, '..', 'views'))
-
   app.use('/upload', upload)
   app.use('/stream', stream)
-  app.use('/embed', embed)
-  app.use('/oembed', oembed)
 
   app.get('*', (req, res) => handle(req, res))
 
