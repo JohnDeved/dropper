c3df3f0f969a063fc575a9c9928092be29417bdc
Undefined <johann@objekt.stream>
2020-07-19 04:39:11 +0200

    + use oembed route

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import upload from './routes/upload'
 import stream from './routes/stream'
 import * as Sentry from '@sentry/node';
 import { nextjs, handle } from './modules/next';
+import oembed from './routes/oembed';
 
 Sentry.init({
   dsn: 'https://0ec6c589070e455c971972cb634fb8fc@sentry.up1.dev/4'
@@ -16,6 +17,7 @@ nextjs.prepare().then(() => {
 
   app.use('/upload', upload)
   app.use('/stream', stream)
+  app.use('/oembed', oembed)
 
   app.get('*', (req, res) => handle(req, res))
 
