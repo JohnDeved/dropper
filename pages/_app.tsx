8c89c765da943940f2f24aa8f2918c639ad9cfbf
halykon <sicstar21@gmail.com>
2020-07-20 17:47:50 +0200

    + new favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,10 @@ export default function MyApp({ Component, pageProps }) {
       <title>Dropper</title>
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600&display=swap" rel="preload" as="style"/>
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600&display=swap" rel="stylesheet"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+      <link rel="manifest" href="/site.webmanifest"></link>
     </Head>
     <Component {...pageProps} />
   </>
