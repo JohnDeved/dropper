1020dc494bf25e5061487a476add5dc8bc69ab26
Undefined <johann@objekt.stream>
2020-08-15 15:40:22 +0200

    + smaller shadow

--- pages/embed/[filename].tsx
+++ pages/embed/[filename].tsx
@@ -138,12 +138,12 @@ export default function Embed ({ filename, name, size }: IProps) {
           border-radius: 20px 20px 5px 5px;
           overflow: hidden;
           box-shadow:
-            0 2.8px 2.2px rgba(0, 0, 0, 0.02),
-            0 6.7px 5.3px rgba(0, 0, 0, 0.028),
-            0 12.5px 10px rgba(0, 0, 0, 0.035),
-            0 22.3px 17.9px rgba(0, 0, 0, 0.042),
-            0 41.8px 33.4px rgba(0, 0, 0, 0.05),
-            0 100px 80px rgba(0, 0, 0, 0.07)
+            0 0.7px 2.2px rgba(0, 0, 0, 0.02),
+            0 1.7px 5.3px rgba(0, 0, 0, 0.028),
+            0 3.1px 10px rgba(0, 0, 0, 0.035),
+            0 5.6px 17.9px rgba(0, 0, 0, 0.042),
+            0 10.4px 33.4px rgba(0, 0, 0, 0.05),
+            0 25px 80px rgba(0, 0, 0, 0.07)
           ;
         }
       `}</style>
