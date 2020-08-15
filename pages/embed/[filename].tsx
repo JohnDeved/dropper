a9332a146f52fa290c1325b0978e09a1c3c1229b
Undefined <johann@objekt.stream>
2020-08-15 15:36:04 +0200

    + style download button

--- pages/embed/[filename].tsx
+++ pages/embed/[filename].tsx
@@ -49,8 +49,11 @@ export default function Embed ({ filename, name, size }: IProps) {
     if (filename) {
       return (
         <div className="content">
-          <meta httpEquiv="Refresh" content={`0; url='${streamRoute}'`} />
-          <Button href={streamRoute} target="_blank" appearance="primary">Download</Button>
+          {/* <meta httpEquiv="Refresh" content={`0; url='${streamRoute}'`} /> */}
+          <div className="download">
+            <img height="150px" src={`/stream/thumb/${filename}`}/>
+            <Button style={{ borderRadius: 0 }} href={streamRoute} target="_blank" appearance="primary">Download ({size})</Button>
+          </div>
         </div>
       )
     }
@@ -121,11 +124,27 @@ export default function Embed ({ filename, name, size }: IProps) {
         html,body {
           width: 100%;
           height: 100%;
+          background-color: transparent !important;
         }
 
         .content {
           display: flex;
           justify-content: center;
+        }
+
+        .download {
+          display: flex;
+          flex-direction: column;
+          border-radius: 20px 20px 5px 5px;
+          overflow: hidden;
+          box-shadow:
+            0 2.8px 2.2px rgba(0, 0, 0, 0.02),
+            0 6.7px 5.3px rgba(0, 0, 0, 0.028),
+            0 12.5px 10px rgba(0, 0, 0, 0.035),
+            0 22.3px 17.9px rgba(0, 0, 0, 0.042),
+            0 41.8px 33.4px rgba(0, 0, 0, 0.05),
+            0 100px 80px rgba(0, 0, 0, 0.07)
+          ;
         }
       `}</style>
     </Head>
