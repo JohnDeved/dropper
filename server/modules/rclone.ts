c4b560eb3920067c4438c51405430c0195be18a8
Undefined <johann@objekt.stream>
2020-08-18 09:05:47 +0200

    + cache test

--- server/modules/rclone.ts
+++ server/modules/rclone.ts
@@ -9,9 +9,9 @@ export function serve () {
   const serve = spawn(rclone, [
     '--config=./rclone.conf', 'serve', 'http', 'dropper:', '-vv',
     '--baseurl', '/stream',
-    '--poll-interval', '5s',
-    '--cache-dir', 'tmp/cache',
-    '--vfs-cache-mode', 'full'
+    '--poll-interval', '15s',
+    '--cache-dir', 'tmp/cache'
+    // '--vfs-cache-mode', 'full'
   ])
   serve.stdout.on('data', log)
   serve.stderr.on('data', log)
